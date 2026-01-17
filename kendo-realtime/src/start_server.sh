80279f51da68061cdc28ab1fa8bcbabdb71c6690
mizuyoshi0829 <mizu@pop01.odn.ne.jp>
2026-01-18 06:46:43 +0900

    20260118

--- kendo-realtime/src/start_server.sh
+++ kendo-realtime/src/start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# nohup /usr/bin/php /var/www/cgi-bin/kendo-realtime/src/wsServer.php &> /dev/null &
-# nohup /usr/bin/php /var/www/cgi-bin/kendo-realtime/src/ws2Server.php &> /dev/null &
-# nohup /usr/bin/php /var/www/cgi-bin/kendo-realtime/src/realtime_queue.php &> /dev/null &
-# nohup /usr/bin/php /var/www/cgi-bin/kendo-realtime/src/pdf_queue.php &> /dev/null &
+nohup /usr/bin/php /var/www/cgi-bin/kendo-realtime/src/wsServer.php &> /dev/null &
+nohup /usr/bin/php /var/www/cgi-bin/kendo-realtime/src/ws2Server.php &> /dev/null &
+nohup /usr/bin/php /var/www/cgi-bin/kendo-realtime/src/realtime_queue.php &> /dev/null &
+nohup /usr/bin/php /var/www/cgi-bin/kendo-realtime/src/pdf_queue.php &> /dev/null &
